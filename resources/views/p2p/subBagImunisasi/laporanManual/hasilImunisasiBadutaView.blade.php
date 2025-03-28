18b12465a98487606fe1eb722310f3eb288d3085
Muhammad Fathurraihan Saputra <fthrn.s14@gmail.com>
2025-03-28 19:37:12 +0700

    UI: Fixing Add Modal Form di hasilImunisasiBadutaView.blade.ph

--- resources/views/p2p/subBagImunisasi/laporanManual/hasilImunisasiBadutaView.blade.php
+++ resources/views/p2p/subBagImunisasi/laporanManual/hasilImunisasiBadutaView.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
     <div class="px-6 py-6 h-full flex flex-col gap-5 overflow-y-scroll">
-        <h1 class="url-route">Program Pencegahan dan Pengendalian Penyakit / Sub. Bagian Imunisasi / Laporan Manual / Sasaran Baduta</h1>
+        <h1 class="url-route">Program Pencegahan dan Pengendalian Penyakit / Sub. Bagian Imunisasi / Laporan Manual /
+            Sasaran Baduta</h1>
         <div class="border border-gray-100"></div>
         <h1 class="text-2xl font-bold text-center mb-5 mt-5 text-gray-700">Sasaran Baduta</h1>
         <div>
@@ -8,7 +9,8 @@
                 <div class="col-start-1 col-end-3">
                     <label for="provinsi">Kecamatan</label>
                     <div class="flex space-x-4">
-                        <select class="w-full mt-3 border border-gray-300 p-3 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 w-1/4">
+                        <select
+                            class="w-full mt-3 border border-gray-300 p-3 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 w-1/4">
                             <option disabled selected>-- Silahkan Pilih Kecamatan --</option>
                             <option>Kecamatan</option>
                             <option>Karangampel</option>
@@ -18,7 +20,8 @@
                 <div class="col-start-1 col-end-3">
                     <label for="desa">Desa</label>
                     <div class="flex space-x-4">
-                        <select class="w-full mt-3 border border-gray-300 p-3 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 w-1/4">
+                        <select
+                            class="w-full mt-3 border border-gray-300 p-3 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 w-1/4">
                             <option disabled selected>-- Silahkan Pilih Desa --</option>
                             <option>Kecamatan</option>
                             <option>Karangampel</option>
@@ -28,7 +31,8 @@
                 <div class="col-start-1 col-end-3">
                     <label for="puskesmas">Puskesmas</label>
                     <div class="flex space-x-4">
-                        <select class="w-full mt-3 border border-gray-300 p-3 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 w-1/4">
+                        <select
+                            class="w-full mt-3 border border-gray-300 p-3 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 w-1/4">
                             <option disabled selected>-- Silahkan Pilih Puskesmas --</option>
                             <option>Kecamatan</option>
                             <option>Karangampel</option>
@@ -38,7 +42,8 @@
                 <div class="col-start-1 col-end-3 grid grid-cols-2 gap-3">
                     <div>
                         <label for="bulan">Bulan</label>
-                        <select class="w-full mt-3 border border-gray-300 p-3 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 w-1/4">
+                        <select
+                            class="w-full mt-3 border border-gray-300 p-3 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 w-1/4">
                             <option disabled selected>-- Silahkan Pilih Bulan --</option>
                             <option>Januari</option>
                             <option>Februari</option>
@@ -46,7 +51,8 @@
                     </div>
                     <div>
                         <label for="tahun">Tahun</label>
-                        <select class="w-full mt-3 border border-gray-300 p-3 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 w-1/4">
+                        <select
+                            class="w-full mt-3 border border-gray-300 p-3 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 w-1/4">
                             <option disabled selected>-- Silahkan Pilih Tahun --</option>
                             <option>2024</option>
                             <option>2025</option>
@@ -56,25 +62,29 @@
             </form>
             <div class="flex gap-3 justify-between mt-5">
                 <div class="flex justify-start gap-2">
-                    <button class="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-200 flex items-center cursor-pointer">
+                    <button
+                        class="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-200 flex items-center cursor-pointer">
                         <img src="{{ asset('images/download.png') }}" alt="Export Icon" class="mr-2 w-5 h-5">
                         Export Rekap Data
                     </button>
                 </div>
                 <div class="flex justify-end gap-2">
-                    <button class="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-200 flex items-center cursor-pointer">
+                    <button
+                        class="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-200 flex items-center cursor-pointer">
                         <img src="{{ asset('images/search.png') }}" alt="Search Icon" class="mr-2 w-5 h-5">
                         Cari Data
                     </button>
-                    <button onclick="openForm()" class="bg-green-600 text-white p-3 rounded-lg hover:bg-green-700 transition duration-200 flex items-center cursor-pointer">
+                    <button onclick="openForm()"
+                        class="bg-green-600 text-white p-3 rounded-lg hover:bg-green-700 transition duration-200 flex items-center cursor-pointer">
                         <img src="{{ asset('images/plus.png') }}" alt="Add Icon" class="mr-2 w-5 h-5">
                         Tambah Data
                     </button>
                 </div>
             </div>
         </div>
-        <div id="popupForm" class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center hidden">
-            <div class="bg-white p-6 rounded-lg w-1/2 shadow-lg">
+        <div id="popupForm" class="fixed inset-0 justify-center items-center hidden">
+            <div class="absolute bg-black opacity-50 inset-0"></div>
+            <div class="absolute bg-white p-6 rounded-lg w-1/2 shadow-lg ">
                 <h2 class="text-2xl font-bold mb-4 text-center">Sasaran Baduta</h2>
                 <form action="#" method="POST">
                     <div class="mb-4">
@@ -131,13 +141,15 @@
                         <div class="w-1/2">
                             <div class="mb-4">
                                 <label for="lakiLaki" class="block text-gray-700">Sasaran Baduta Laki-Laki</label>
-                                <input type="number" id="lakiLaki" name="lakiLaki" class="w-full border p-3 rounded-lg" value="5" required>
+                                <input type="number" id="lakiLaki" name="lakiLaki"
+                                    class="w-full border p-3 rounded-lg" value="5" required>
                             </div>
                         </div>
                         <div class="w-1/2">
                             <div class="mb-4">
                                 <label for="perempuan" class="block text-gray-700">Sasaran Baduta Perempuan</label>
-                                <input type="number" id="perempuan" name="perempuan" class="w-full border p-3 rounded-lg" value="5" required>
+                                <input type="number" id="perempuan" name="perempuan"
+                                    class="w-full border p-3 rounded-lg" value="5" required>
                             </div>
                         </div>
                     </div>
@@ -145,20 +157,26 @@
                     <div class="mb-4 flex space-x-4">
                         <div class="w-1/2">
                             <div class="mb-4">
-                                <label for="jumlahLakiLaki" class="block text-gray-700">Jumlah Imunisasi Baduta Laki-Laki</label>
-                                <input type="number" id="jumlahLakiLaki" name="jumlahLakiLaki" class="w-full border p-3 rounded-lg" value="1" required>
+                                <label for="jumlahLakiLaki" class="block text-gray-700">Jumlah Imunisasi Baduta
+                                    Laki-Laki</label>
+                                <input type="number" id="jumlahLakiLaki" name="jumlahLakiLaki"
+                                    class="w-full border p-3 rounded-lg" value="1" required>
                             </div>
                         </div>
                         <div class="w-1/2">
                             <div class="mb-4">
-                                <label for="jumlahPerempuan" class="block text-gray-700">Jumlah Imunisasi Baduta Perempuan</label>
-                                <input type="number" id="jumlahPerempuan" name="jumlahPerempuan" class="w-full border p-3 rounded-lg" value="2" required>
+                                <label for="jumlahPerempuan" class="block text-gray-700">Jumlah Imunisasi Baduta
+                                    Perempuan</label>
+                                <input type="number" id="jumlahPerempuan" name="jumlahPerempuan"
+                                    class="w-full border p-3 rounded-lg" value="2" required>
                             </div>
                         </div>
                     </div>
                     <div class="flex justify-end space-x-4">
-                        <button type="button" onclick="closeForm()" class="bg-gray-400 text-white p-3 rounded-lg hover:bg-gray-500">Cancel</button>
-                        <button type="submit" class="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700">Save</button>
+                        <button type="button" onclick="closeForm()"
+                            class="bg-gray-400 text-white p-3 rounded-lg hover:bg-gray-500">Cancel</button>
+                        <button type="submit"
+                            class="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700">Save</button>
                     </div>
                 </form>
             </div>
@@ -207,12 +225,13 @@
                         <img src="{{ asset('images/edit.png') }}" alt="Edit" class="w-6 h-6 cursor-pointer">
                         <img src="{{ asset('images/trash.png') }}" alt="Delete" class="w-6 h-6 cursor-pointer">
                     </td>
-                 </tr>
+                </tr>
             </tbody>
         </table>
         <div class="flex gap-3 justify-between mt-5">
             <div class="flex justify-start gap-2">
-                <button class="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-200 flex items-center cursor-pointer">
+                <button
+                    class="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-200 flex items-center cursor-pointer">
                     <img src="{{ asset('images/download.png') }}" alt="Export Icon" class="mr-2 w-5 h-5">
                     Export Rekap Data
                 </button>
@@ -238,8 +257,8 @@
                     <td class="border px-4 py-2">Januari</td>
                     <td class="border px-4 py-2 text-center">5</td>
                     <td class="border px-4 py-2 text-center">5</td>
-                 </tr>
-                 <tr class="border">
+                </tr>
+                <tr class="border">
                     <td class="border px-4 py-2 text-center">2</td>
                     <td class="border px-4 py-2">Februari</td>
                     <td class="border px-4 py-2 text-center">5</td>
@@ -259,6 +278,7 @@
     <script>
         function openForm() {
             document.getElementById('popupForm').classList.remove('hidden');
+            document.getElementById('popupForm').classList.add("flex");
         }
 
         function closeForm() {
@@ -266,5 +286,3 @@
         }
     </script>
 </x-layout>
-
-
